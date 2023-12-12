3cebd8da187ab5d07b6a9ed40da56ff12c9aaf80
eliana osorio <yasminosorio2013@gmail.com>
2023-12-12 18:32:37 -0500

    feat: functions

--- javaScript/clase1.js
+++ javaScript/clase1.js
@@ -39,7 +39,7 @@ let edades = [30, 33, 5,2];
 //objetos
 let persona = {
     nombre: "eli",
-    edad: 30,
+    edad: 20,
     sexo: "F",
     esMayorDeEdad: true,
     puntajeDatacredito: null,
@@ -69,66 +69,100 @@ if (edadPersona >= mayorDeEdad){
 }
 
 //condiciones varias,
-const edadPensionM = 62;
-const edadPensionF = 57;
 
-if(persona.edad >=edadPensionF && persona.sexo === "F" && persona.semanasCotizadas >= 1300){
-    console.log("reclame su pension");
-}else if (persona.edad >= edadPensionM && persona.sexo === "M" && persona.semanasCotizadas >= 1300) {
-    console.log("vaya por su pension");
-}else {
-    console.log("se quedo sin pension");
+
+//do while garantiza que se cumpla al menos 1 ves la condicion en el bloque.
+
+/* do{
+    console.log("bienvenid@s a Mario karts");
+    console.log("1. jugador");
+    console.log("2. configuracion");
+    console.log("3. Multi juadores");
+    console.log("4. salir");
+
+    let opcion = prompt("selecciona una opcion");
+}while(opcion != 4); */
+
+//Funciones 
+function saludar(){
+    console.log("Bienveido a este lugar");
+if (persona.edad > 18) {
+    console.log("puede jugar");
 }
-
-// condicion con el operador or ||
-
-if(persona.cedula || persona.tienePasaporte) {
-    console.log("puede salir del pais");
-}else {
-    console.log("no puede salir del pais");
 }
-// switch case
-let dia = "sabado";
+saludar();
 
-switch(dia){
-    case "lunes":
-        console.log("Hoy es lunes");
-    break;
-    case "martes":
-        console.log("Hoy es martes");
-    break;
-    case "miercoles":
-        console.log(" Hoy es miercoles");
-    break;
-    case "jueves":
-        console.log(" Hoy es jueves");
-    break;
-    case "viernes":
-        console.log("Hoy es viernes");
-    break;
-    case "sabado":
-        console.log("Hoy es sabado");
-    break;
-    case "domingo":
-        console.log("Hoy es domingo");
-    break;
-    default:
-        console.log("no es un dia de la semana");
-}
-//operador ternario
+const condiciones = () => {
+    const edadPensionM = 62;
+    const edadPensionF = 57;
 
-persona.sexo == "M" ? console.log("es un hombre") : console.log(" es una mujer");
-// ciclos //  bucles, loops 
-//For
-console.log("un elefante se balanceaba sobre la tela de una araña, como veia que resisitia fue a llamar a otro elefante");
-console.log("do elefante se balanceaban sobre la tela de una araña, como veian que resisitia fue a llamar a otro elefante");
-console.log("tres elefante se balanceaban sobre la tela de una araña, como veian que resisitia fue a llamar a otro elefante");
-console.log("cuatro elefante se balanceaban sobre la tela de una araña, como veia que resisitia fue a llamar a otro elefante");
-console.log("cinco elefante se balanceaban sobre la tela de una araña, como veia que resisitia fue a llamar a otro elefante");
-for (let contador = 1; contador <= 5; contador++){
-    if (contador === 1) {
-        console.log(contador + " elefante se balanceaba sobre la tela de una araña, como veia que resisitia fue a llamar a otro elefante");
-    }else{
-    console.log(contador + " elefantes se balanceaban sobre la tela de una araña, como veian que resisitia fue a llamar a otro elefante");
+    if(persona.edad >=edadPensionF && persona.sexo === "F" && persona.semanasCotizadas >= 1300){
+        console.log("reclame su pension");
+    }else if (persona.edad >= edadPensionM && persona.sexo === "M" && persona.semanasCotizadas >= 1300) {
+        console.log("vaya por su pension");
+    }else {
+        console.log("se quedo sin pension");
+    }
+
+    // condicion con el operador or ||
+
+    if(persona.cedula || persona.tienePasaporte) {
+        console.log("puede salir del pais");
+    }else {
+        console.log("no puede salir del pais");
+    }
+    // switch case
+    let dia = "sabado";
+
+    switch(dia){
+        case "lunes":
+            console.log("Hoy es lunes");
+        break;
+        case "martes":
+            console.log("Hoy es martes");
+        break;
+        case "miercoles":
+            console.log(" Hoy es miercoles");
+        break;
+        case "jueves":
+            console.log(" Hoy es jueves");
+        break;
+        case "viernes":
+            console.log("Hoy es viernes");
+        break;
+        case "sabado":
+            console.log("Hoy es sabado");
+        break;
+        case "domingo":
+            console.log("Hoy es domingo");
+        break;
+        default:
+            console.log("no es un dia de la semana");
+    }
+    //operador ternario
+
+    persona.sexo == "M" ? console.log("es un hombre") : console.log(" es una mujer");
+    // ciclos //  bucles, loops 
+    //For
+    console.log("un elefante se balanceaba sobre la tela de una araña, como veia que resisitia fue a llamar a otro elefante");
+    console.log("do elefante se balanceaban sobre la tela de una araña, como veian que resisitia fue a llamar a otro elefante");
+    console.log("tres elefante se balanceaban sobre la tela de una araña, como veian que resisitia fue a llamar a otro elefante");
+    console.log("cuatro elefante se balanceaban sobre la tela de una araña, como veia que resisitia fue a llamar a otro elefante");
+    console.log("cinco elefante se balanceaban sobre la tela de una araña, como veia que resisitia fue a llamar a otro elefante");
+    for (let contador = 5; contador <= 5; contador++){
+        console.log(contador + " elefantes se balanceaban sobre la tela de una araña, como veian que resisitia fue a llamar a otro elefante");
+    }
+
+    for (let i = 0; i < nombres.length; i++) {
+        console.log(nombres[i]);
+    }
+    console.log(nombres[3]);
+
+    //ciclo while 
+    while(persona.edad < 30){
+        console.log("para ser treinton le falta " + (30 - persona.edad)+ "  años");
+        persona.edad++; // de uno en uno 
     }
 }
+
+condiciones();
