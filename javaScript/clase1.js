259792aafe2f06022acb33bcbd76bf0a54c42b15
eliana osorio <yasminosorio2013@gmail.com>
2023-12-12 16:29:23 -0500

    feat: js tipos de datos primitivos

---
+++ javaScript/clase1.js
@@ -0,0 +1,32 @@
+//Formas d ecrear una variable
+nombre = "eli";
+
+var nombre = "eli"// variable global se puede usar desde cualquier parte.
+
+let nombre = "eli";
+
+const nombre = "eli"//Constante no se puede reasignar. 
+
+//Tipos de datos primitivos
+
+//String guarda alfa numerico
+
+let nombre ="eliana osorio"
+let dreccion = "calle 33 numero 12b-32"
+let correo = "correo@gmail.com"
+// numbers / numero 
+
+let edad = 30;
+let peso = 64.30;
+let telefono = 12345567;
+
+//Boolean
+
+let esMayorDeEdad = true;
+let esMenorDeEdad = false;
+
+//undefined
+let reporteDataCredito = undefined;
+let reporteSisben;
+//null
+let puntajeDatacredito = null;
