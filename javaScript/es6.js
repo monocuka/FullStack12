b593548c612d1e2787107b723a4a0e4582add017
eliana osorio <yasminosorio2013@gmail.com>
2023-12-15 11:57:52 -0500

    feat: arrow function

--- javaScript/es6.js
+++ javaScript/es6.js
@@ -2,20 +2,21 @@
 //Operador Ternario, template Literals. arrow function
 
 function inicio(){
-
+    function saludar(nombre){
+        return "hola " + nombre;
+    }
+    const saludar = (nombre) => `holas ${nombre}`; 
+    
+    
+    function calcularNacimiento(edad){
+        return 2023 - edad;
+    }
+    let elNombre= prompt("ingresa tu nombre");
+    
+    let edadPersona = prompt("Ingresa tu edad");
+    
+    
+    let result = `${saludar(elNombre)} tu año de nacimiento es ${calcularNacimiento(edadPersona)}`;
 }
+inicio();
 
-function saludar(nombre){
-    return "hola " + nombre;
-}
-
-function calcularNacimiento(edad){
-    return 2023 - edad;
-}
-let elNombre= prompt("ingresa tu nombre");
-let resultadoSaludo = saludar(elNombre);
-
-let edadPersona = prompt("Ingresa tu edad");
-let resultadoCalculo = calcularNacimiento(edadPersona);
-
-console.log(resultadoSaludo + " tu año de nacimiento es " + resultadoCalculo);
