d7313f5dc98c25c7211d50fe8fb3548a597cf3d6
eliana osorio <yasminosorio2013@gmail.com>
2023-12-27 15:55:13 -0500

    feat: interface

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // para crear una clase lo que se hace es poner los atributos y sin son privados 
+var operaciones = require("./Calculadora");
 var Persona = /** @class */ (function () {
     function Persona(nombre, edad) {
         this.nombre = nombre;
@@ -10,5 +13,22 @@ var Persona = /** @class */ (function () {
     };
     return Persona;
 }());
+var empleadoImpl = /** @class */ (function () {
+    function empleadoImpl(nombre, salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+        this.getAntiguedad = function () { return 10; };
+    }
+    empleadoImpl.prototype.pedirAumento = function () {
+        console.log("necesito un aumento jefe");
+    };
+    return empleadoImpl;
+}());
+var empleado1 = new empleadoImpl('ponky', 1000);
+empleado1.pedirAumento();
 var persona1 = new Persona('eli', 30);
 persona1.saludar();
+console.log(operaciones.sumar(3, 6));
+console.log(operaciones.restar(29, 8));
+console.log(operaciones.multiplicar(4, 6));
+console.log(operaciones.dividir(15, 3));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 // para crear una clase lo que se hace es poner los atributos y sin son privados 
+import * as operaciones from './Calculadora';
+import Empleado from './empleado'
+
 class Persona {
     constructor(public nombre: string, public edad: number){
     }
@@ -9,6 +12,21 @@ class Persona {
         console.log(`Hola soy ${this.nombre} y tengo ${this.edad} años y mi numero de la suerte es ${this.numeroDeLaSuerte()}`);
     }
 }
+class empleadoImpl implements Empleado{
+    constructor(public nombre: string, public salario: number,){}
+    pedirAumento(): void {
+        console.log("necesito un aumento jefe");
+        
+    }
+    getAntiguedad = (): number => 10;
+}
+const empleado1 = new empleadoImpl('ponky', 1000)
+empleado1.pedirAumento();
 
 const persona1 = new Persona('eli', 30);
 persona1.saludar();
+
+console.log(operaciones.sumar(3, 6));
+console.log(operaciones.restar(29, 8));
+console.log(operaciones.multiplicar(4, 6));
+console.log(operaciones.dividir(15, 3));
